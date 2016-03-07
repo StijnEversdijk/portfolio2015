9d25bbd57940f9a49c64483dde773fedd8a278d4
Stijn Eversdijk <stijn@statlerandsons.com>
2016-03-07 22:20:02 +0100

    remove iuxd project

--- partials/general/work.php
+++ partials/general/work.php
@@ -3,6 +3,6 @@
   <?php include 'partials/work/short/kuvva.php' ?>
   <?php include 'partials/work/short/statler.php' ?>
   <?php include 'partials/work/short/amongstfriends.php' ?>
-  <?php include 'partials/work/short/iuxd.php' ?>
+  <?php // include 'partials/work/short/iuxd.php' ?>
 
 </section>
